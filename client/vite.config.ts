53a6de41cb8ed679eaf482297a0e857a21e36e10
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-04-04 13:37:57 +0700

    add bun

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
